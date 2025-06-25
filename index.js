016836a11d057a43bdc17b58db45ef9eae605b41
sibuthejhb024 <sibuthejhb024@student.wethinkcode.co.za>
2025-06-25 03:08:05 +0200

    Convert button functionality

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.getElementById("header1").textContent = "Temperature Converter";
 const unitSelector = document.getElementById("unitSelector");
 const inputTextBox = document.getElementById("inputTemp");
 const convertButton = document.getElementById("convertButton");
-let inputIsValid = false;
+let outputDisplay = document.getElementById("outputP");
 let celsiusValue;
 let fahrenheitValue;
 // tried using if statements to change an element's text content upon clicking the element, which doesn't work
@@ -31,17 +31,27 @@ unitSelector.onclick = function(){
 
 // this is where our calculations will take place
 convertButton.onclick = function(){
-    while (inputIsValid == false) {
+    // var inputIsValid = (typeof Number(inputTextBox.value) == "number");
+    // credit for line 37: ChatGPT
+    var inputIsValid = !isNaN(Number(inputTextBox.value)) && inputTextBox.value.trim() !== "";
+    console.log(typeof inputTextBox.value);
+    console.log(typeof Number)
+    console.log(typeof Number(inputTextBox.value));
+    console.log(inputIsValid);
+    if (inputIsValid == false) {
         // alert("Please enter a numberic value.");
+        inputTextBox.value = "";
         inputTextBox.placeholder = "Please enter a number.";
-        if (typeof Number(inputTextBox.value) == Number){
-            if (unitSelector.textContent == "F"){
-                fahrenheitValue = Number(inputTextBox.value);
-                celsiusValue = (fahrenheitValue - 32) * 5/9;
-            } else {
-                celsiusValue = Number(inputTextBox.value);
-                fahrenheitValue = (celsiusValue * 9/5) + 32;
-            }
+        outputDisplay.textContent = "";
+    } else {
+        if (unitSelector.textContent == "F"){
+            fahrenheitValue = Number(inputTextBox.value);
+            celsiusValue = (fahrenheitValue - 32) * 5/9;
+            outputDisplay.textContent = celsiusValue + "C";
+        } else {
+            celsiusValue = Number(inputTextBox.value);
+            fahrenheitValue = (celsiusValue * 9/5) + 32;
+            outputDisplay.textContent = fahrenheitValue + "F";
         }
     }
 }
